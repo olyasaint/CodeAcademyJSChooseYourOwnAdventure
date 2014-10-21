fa54eef89c7acc96c36165f24a46afe1544cb4b1
olyasaint <olya@ostryk.com>
2014-10-21 11:26:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+/ Check if the user is ready to play!
+confirm("Ready?")
 
+// Prompt user and check their age.
 var age=prompt("What's your age?")
 if(age<13){console.log("You are too young to play")}else{console.log("Go on and play!")}
 console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'")
